d1971c276fc727c34800c61d3d638263fbeb6bba
gustavo2892 <gustavo2892@gmail.com>
2022-07-31 17:15:01 -0300

    Adjust Button Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,40 @@
+import { forwardRef, AnchorHTMLAttributes, ButtonHTMLAttributes } from 'react';
 import * as S from './styles';
 
-const Button = () => (
-  <S.Wrapper>
-    <h1>Button</h1>
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>;
+
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+  minimal?: boolean;
+  icon?: JSX.Element;
+  as?: React.ElementType;
+} & ButtonTypes;
+
+const Button: React.ForwardRefRenderFunction<S.WrapperProps, ButtonProps> = (
+  {
+    children,
+    icon,
+    size = 'medium',
+    fullWidth = false,
+    minimal = false,
+    ...props
+  },
+  ref
+) => (
+  <S.Wrapper
+    size={size}
+    fullWidth={fullWidth}
+    hasIcon={!!icon}
+    minimal={minimal}
+    ref={ref}
+    {...props}
+  >
+    {icon}
+    {!!children && <span>{children}</span>}
   </S.Wrapper>
 );
 
-export default Button;
+export default forwardRef(Button);
